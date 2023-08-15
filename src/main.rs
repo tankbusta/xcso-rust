b4cadfeb5ed0332fa7e9b6b7d944c36877a8216d
Christopher Schmitt <cschmitt@tankbusta.net>
2023-08-14 19:24:32 -0600

    fix: refactored how lz4 works to get it closer to py

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use std::io;
 use std::ffi::OsString;
 use std::io::{Read, Seek};
 use std::path::Path;
-use byteorder::{WriteBytesExt, LittleEndian};
-use lz4_flex::frame::{FrameInfo, BlockMode};
 
+use byteorder::{WriteBytesExt, LittleEndian};
 use console::{style, Emoji};
 use indicatif::ProgressBar;
+use minilz4::EncoderBuilder;
 
 static CISO_MAGIC: u32 = 0x4F534943; // CISO
 static CISO_HEADER_SIZE: u32 = 0x18; // 24
@@ -88,6 +88,9 @@ fn get_cso_info(f: &mut File) -> Result<CsoImage, io::Error> {
 
     let byte_len: u64 = fmetadata.len() - image_offset as u64;
     let blocks: usize = (byte_len as usize/ CISO_BLOCK_SIZE) as usize;
+
+    f.seek(io::SeekFrom::Start(image_offset as u64))?;
+
     return Ok(CsoImage {
         version: 2,
         align: 2,
@@ -98,18 +101,18 @@ fn get_cso_info(f: &mut File) -> Result<CsoImage, io::Error> {
 
 fn write_cso_info(f: &mut File, img_data: CsoImage) -> Result<(), Error> {
     let mut buf: Vec<u8> = Vec::new();
-    buf.write(&CISO_MAGIC.to_le_bytes()).unwrap();
-    buf.write(&CISO_HEADER_SIZE.to_le_bytes()).unwrap();
-    buf.write(&img_data.total_bytes.to_le_bytes()).unwrap();
+    buf.write(&CISO_MAGIC.to_le_bytes())?;
+    buf.write(&CISO_HEADER_SIZE.to_le_bytes())?;
+    buf.write(&img_data.total_bytes.to_le_bytes())?;
 
     let block_size = CISO_BLOCK_SIZE as u32;
-    buf.write(&block_size.to_le_bytes()).unwrap();
+    buf.write(&block_size.to_le_bytes())?;
 
-    buf.write_u8(img_data.version).unwrap();
-    buf.write_u8(img_data.align).unwrap();
+    buf.write_u8(img_data.version)?;
+    buf.write_u8(img_data.align)?;
 
     let pad: u16 = 0;
-    buf.write(&pad.to_le_bytes()).unwrap();
+    buf.write(&pad.to_le_bytes())?;
 
     assert_eq!(CISO_HEADER_SIZE, buf.len() as u32);
     f.write_all(&buf)
@@ -127,20 +130,23 @@ fn write_block_index(f: &mut File, blocks: &Vec<u32>) -> Result<u64, Error> {
     return f.seek(io::SeekFrom::Current(0));
 }
 
-fn compress_block(block: Vec<u8>) -> Result<Vec<u8>, Error> {
-    let uncompressed_size = block.len();
-    let compressed_data: Vec<u8> = Vec::new();
 
-    let mut frame_info = FrameInfo::new();
-    frame_info.content_size = Some(uncompressed_size as u64);
-    frame_info.block_checksums = false;
-    frame_info.content_checksum = false;
-    frame_info.block_mode = BlockMode::Independent;
+fn compress_block_v2(block: Vec<u8>) -> Result<Vec<u8>, Error> {
+    let mut encoder = EncoderBuilder::new().
+        auto_flush(true).
+        checksum(minilz4::ContentChecksum::NoChecksum).
+        block_mode(minilz4::BlockMode::Independent).
+        level(16).
+        build(Vec::new()).unwrap();
+    {
+        std::io::Write::write_all(&mut encoder, &block)?;
+    }
 
-    let mut enc = lz4_flex::frame::FrameEncoder::with_frame_info(frame_info, compressed_data);
-
-    std::io::Write::write_all(&mut enc, &block)?;
-    Ok(enc.finish()?)
+    let result = encoder.finish()?;
+    // TODO: Investigate why there's (likely) a checksum and the size header
+    // despite turning checksum mode off
+    return Ok(result[11..].to_vec());
+    // return Ok(result);
 }
 
 fn compress_iso(fp: &String) -> Result<String, io::Error> {
@@ -166,7 +172,7 @@ fn compress_iso(fp: &String) -> Result<String, io::Error> {
 
     let align_b = 1 << image_details.align;
     let align_m = align_b - 1;
-    let alignment_buffer: Vec<u32> = vec![0; 64];
+    let alignment_buffer: Vec<u8> = vec![0; 64];
 
     // Holds the block size
     let mut blockbuf = vec![0; CISO_BLOCK_SIZE];
@@ -174,26 +180,24 @@ fn compress_iso(fp: &String) -> Result<String, io::Error> {
 
     for block in 0..image_details.total_blocks {
         // Check if we need to split the ISO (due to FATX limitations)
-		// TODO: Determine a better value for this.
+        // TODO: Determine a better value for this.
         if write_pos > 0xFFBF6000 {
             panic!("TODO: Split support")
         }
 
-        let mut align: u64 = write_pos & align_m;
+        let mut align: usize = write_pos as usize & align_m as usize;
         if align > 0 {
             align = align_b - align;
-            for b in alignment_buffer.iter().take(align as usize) {
-                _ = dest_f1.write_u32::<LittleEndian>(*b)
-            }
-            write_pos += align;
+            dest_f1.write_all(&alignment_buffer[..align])?;
+            write_pos += align as u64;
         }
 
         block_index[block] = write_pos as u32 >> image_details.align as u32;
         let read = iso_file.read(&mut blockbuf[..])?;
-        let compressed = compress_block(blockbuf[..read].to_vec())?;
+        let compressed = compress_block_v2(blockbuf[..read].to_vec())?;
 
         // If the compressed size is greater than the original, prefer the original
-        if compressed.len() + 12 > read {
+        if compressed.len() + 12 >= read {
             write_pos += read as u64;
             dest_f1.write(&blockbuf[..read])?;
         } else {
@@ -206,11 +210,12 @@ fn compress_iso(fp: &String) -> Result<String, io::Error> {
     }
 
     // end for block
-	// last position (total size)
-	// NOTE: We don't actually need this, but we're keeping it for legacy reasons.
+    // last position (total size)
+    // NOTE: We don't actually need this, but we're keeping it for legacy reasons.
     let last = block_index.len()-1;
     block_index[last] = write_pos as u32 >> image_details.align as u32;
 
+    // Seek back to the beginning, past the header to re-write the block index
     dest_f1.seek(io::SeekFrom::Start(CISO_HEADER_SIZE as u64))?;
     write_block_index(&mut dest_f1, &block_index)?;
 
